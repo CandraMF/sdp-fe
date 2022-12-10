fff2f456fad6eb09262f1c2707321532276bc70b
ANDRIYANA <andri15051988@gmail.com>
2022-12-10 14:42:33 +0700

    migrate-nullable pembinaan kepribadian

--- database/migrations/2022_11_12_151105_PembinaanKepribadianTable.php
+++ database/migrations/2022_11_12_151105_PembinaanKepribadianTable.php
@@ -15,14 +15,14 @@
 				$table->string('nama_program',200);
 				$table->boolean('program_wajib');
 				$table->string('materi_pembinaan_kepribadian',200);
-				$table->string('id_instruktur',32);
+				$table->string('id_instruktur',32)->nullable();
 				$table->string('penangung_jawab',32);
 				$table->date('tanggal_mulai');
 				$table->date('tanggal_selesai');
 				$table->string('tempat_pelaksanaan',200);
 				$table->boolean('perlu_kelulusan');
-				$table->string('id_sarana',32);
-				$table->string('id_prasarana',32);
+				$table->string('id_sarana',32)->nullable();
+				$table->string('id_prasarana',32)->nullable();
 				$table->decimal('realisasi_anggaran',18,2)->nullable();
 				$table->string('id_jenis_anggaran',32)->nullable();
 				$table->string('foto',200);
